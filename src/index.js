5c0788d90b47e91c7581cbaa0f0af4aa6219efa4
Cristian Muscalu <muscaiu@yahoo.com>
2020-02-05 22:08:03 +0000

    sending data to the FE

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import 'dotenv/config';
-const io = require('socket.io')();
 
 import livingScanner from './scanners/livingScanner';
 import bathroomScanner from './scanners/bathroomScanner';
 import vladScanner from './scanners/vladScanner';
 import kitchenScanner from './scanners/kitchenScanner';
-
 import {tempCron} from './crons/tempCron';
 import models, { sequelize } from './models';
+
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 tempCron.start();
 
@@ -21,13 +23,20 @@ io.on('connection', (client) => {
 
   client.on('toggleLiving', () => {
     var spawn = require("child_process").spawn;
-    
     const process = spawn('python', ["./livolo.py", 'on']);
-    
     process.stdout.on('data', function (data) {
       console.log(data.toString())
     })
   });
+
+  client.on('getTemperatures',  async() => {
+    const tremperatures = await models.Temperature.findAll({
+      limit: 100,
+      where: {},
+      order: [['createdAt', 'ASC']]
+    })
+    client.emit('temperatures', tremperatures)
+  })
 
   sequelize.sync().then(async () => {
     console.log('+++ DB connected')
@@ -35,4 +44,4 @@ io.on('connection', (client) => {
   
 });
 
-io.listen(process.env.PORT);
+server.listen(process.env.PORT);
